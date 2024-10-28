a3e2410e616e3435ec6131488cd3d65c656bdfb6
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-10-28 10:53:25 +0700

    update

--- src/core/routes/register.route.ts
+++ src/core/routes/register.route.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response, Router } from "express";
+import { Router } from "express";
 import { getControllerPath } from "../../decorators/controller.decorator";
 import { combinePaths } from "../../utils/common";
 import { getRouteMetadata } from "../../decorators/method.decorator";
